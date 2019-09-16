41b15e0be11c66adb2db0b2b08fac7a4f27e9c03
Alol0007 <alexx.olavarria@gmail.com>
2019-09-16 09:48:57 +0100

    Register and Login form + easier validation

--- demo-app/README.md
+++ demo-app/README.md
@@ -1,4 +1,5 @@
-Installera -    npm create-react-app demo-app -     i kursreport jsramverk.
-Lagt till Route -    import { BrowserRouter as Router, Route, Link } from "react-router-dom"; i App.js samt funktionen function AppRouter() {}.
-.env fil -  Skapat en fil för att applikationen skall gå igenom PORT=3005 istället för 3000 default.
-Kör igång - Npm start
+<p>Installera -    npm install</p>
+
+<p>.env fil -  Skapat en fil för att applikationen skall gå igenom PORT=3005 istället för 3000 default.</p>
+
+<p>Kör igång - Npm start</p>
--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 //denna e till för router
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+//denna till för formen
+import Login from "./Login";
+import Signup from "./Signup";
+
+
 
 function Index() {
   return App();
 }
+
 
 function About() {
   return <h2>About</h2>;
@@ -17,20 +23,28 @@ function Reports() {
   return whatweek();
 }
 
+
+
+
 function Week() {
     return (
        <div className="Apptext">
        <p>1 Installera -    npm create-react-app demo-app -     i kursreport jsramverk.</p>
        <p>2 Lagt till Route -    import  BrowserRouter as Router, Route, Link  from "react-router-dom"; i App.js samt funktionen function AppRouter().</p>
-       <p>3 .env fil -  Skapat en fil för att applikationen skall gå igenom PORT=3002 istället för 3000 default.</p>
+       <p>3 .env fil -  Skapat en fil för att applikationen skall gå igenom PORT=3005 istället för 3000 default.</p>
        <p>4 Kör igång - Npm start</p>
 
-       <a href="">Github</a>
+       <a
+       className="App-link"
+       href="https://github.com/Alol0007/jsramverk1/tree/master/demo-app"
+       target="_blank"
+       rel="noopener noreferrer"
+       >
+       Github
+       </a>
        </div>
-    )
+   );
  }
-
-
 
 
 
@@ -49,24 +63,33 @@ function AppRouter() {
             <li>
               <Link to="/reports/">Reports</Link>
             </li>
+            <li>
+              <Link to="/login/">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup/">Signup</Link>
+            </li>
           </ul>
         </nav>
-
         <Route path="/" exact component={Index} />
         <Route path="/about/" component={About} />
         <Route path="/reports" component={Reports} />
         <Route path="/reports/week" component={Week} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" exact component={Signup} />
+
       </div>
     </Router>
   );
 }
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+
 
 <p>Min me-sida i kursen jsramverk</p>
 
@@ -75,8 +98,6 @@ function App() {
 <p>Så, en presentation av mig själv är en bra början.</p>
 
 <p>Mitt namn är Alex Lopez Olavarria. Jag är född och uppvuxen i Uddevalla, Bohuslän, som tillhör de västliga delarna av Sverige. Jag är intresserad av det mesta och har i yngre dagar spelat fotboll, numera håller jag mig i form i gymmet speciellt intresserad av övningar såsom marklyft, knäböj och bänkpress. Jag har jobbat i de flesta delarna inom vården och jobbar i nuläget som ledsagare samt i Kungshamn åt sammhällsbyggnadsförvaltningen. Programmering har intresserat mig sen jag gick i gymnasiet.</p>
-
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
